781606766ea5732a235f50727ba68bb63ce112c7
KaypeoLoL <marinokolaric@outlook.com>
2016-12-20 00:15:26 +0100

    showcase

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,17 +1,11 @@
 $(document).ready(function() {
-
   $(window).scroll(function(){
     stripScroll();
   });
-
-
-
 });
 
-
 function stripScroll(){
-  var wScroll =$(window).scrollTop();
-  wScroll= wScroll/2;
+  var wScroll =$(window).scrollTop()/2;
 
   $(".pic").css("background-position", "center -" +wScroll+ "px");
 }
